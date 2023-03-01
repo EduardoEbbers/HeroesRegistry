55cbd2a12f1b19739a0280fd50e5130696e2d543
Carlos Ebbers <cel.ebbers@gmail.com>
2023-03-01 18:11:00 -0300

    header

--- HeroesRegistry/src/app/app.module.ts
+++ HeroesRegistry/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- HeroesRegistry/src/app/core/core.module.ts
+++ HeroesRegistry/src/app/core/core.module.ts
@@ -1,13 +1,23 @@
+import { RouterModule } from '@angular/router';
+import { AppMaterialModule } from './../app-material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    AppMaterialModule,
+    FlexLayoutModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
